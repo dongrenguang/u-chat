957784388ac4a6d9e8837a21e3724481b5529adc
Renguang Dong <dongrenguang@gmail.com>
2016-08-15 21:31:04 +0800

    Complete the primary version, that supporting point to point chatting.

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,11 +1,35 @@
-var socket = io();
-$("form").submit(function() {
-    socket.emit("chat message", $("#m").val());
-    $("#m").val("");
-    return false;
+var logined = false;
+var socket = io("/messageCenter");
+var user = {};
+
+socket.on("auth", function() {
+    user.name = window.location.hash.slice(1);
+    socket.emit("login", user, function(result) {
+        if (result) {
+            logined = true;
+        }
+        else {
+            logined = false;
+            alert("Login failed.");
+        }
+    });
 });
 
-socket.on("chat message", function(msg) {
+socket.on("message", function(msg) {
     var $newMessage = $("<li>").text(msg);
     $("#messages").append($newMessage);
+});
+
+$("#message-form").submit(function() {
+    var from = user.name;
+    var to = $(".to > input").val();
+    var message = $(".message > input").val();
+    socket.emit("message", {
+        from: from,
+        to: to,
+        message: message
+    });
+
+    $(".message > input").val("");
+    return false;
 });
